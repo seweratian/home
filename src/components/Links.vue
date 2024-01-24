dd782fc15673ef134f7cc0861dd6f3b17d439b7c
seweratian <42729342+seweratian@users.noreply.github.com>
2024-01-24 15:51:07 +0800

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Plane, LaptopCode, Youtube, Robot, Images, Microsoft } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Plane, LaptopCode, Youtube, Robot, Images, Google } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper";
@@ -73,7 +73,7 @@ const siteIcon = {
   Youtube,
   Robot,
   Images,
-  Microsoft,
+  Google,
 };
 
 // 链接跳转
